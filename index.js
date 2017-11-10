2a3d34ea62f04a37709d8b97465921d3c18da563
icnpdesign <inp@bu.edu>
2017-11-10 16:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file
-const list = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 
-function findMatching(list, name) {
-  return list.filter(function (drivername) {
+function findMatching(drivers, name) {
+  return list.filter(function (drivers, name) {
     return driverName.toLowercase() === name.toLowerCase();
   });
 }
