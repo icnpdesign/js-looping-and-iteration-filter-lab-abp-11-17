68209721d25c5e8f2da52ea597ea12183efe4c1a
icnpdesign <inp@bu.edu>
2017-11-10 16:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
-function findMatching() {
+function findMatching(drivers, name) {
+  for (const driver)
   return drivers;
 }
