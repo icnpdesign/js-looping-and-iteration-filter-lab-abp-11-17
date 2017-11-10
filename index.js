ea0b18df8e14efdbc07474edd82e302ab21a424f
icnpdesign <inp@bu.edu>
2017-11-10 16:42:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,3 @@ function findMatching (list, name) {
   });
 }
 
-function fuzzyMatch (list, partialName) {
-  let lengthOfName = partialName.length;
-  return list.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === partialName;
-  });
-}
-
-function matchName (list, name) {
-  return list.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
-  });
-}
