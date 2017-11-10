a60e79e6a9eeec7a567add99572e8d1c7faca38f
icnpdesign <inp@bu.edu>
2017-11-10 16:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Code your solution in this file
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 
-function findMatching(drivers, driverName) {
-  return list.filter(function (drivers, driverName) {
-    return driverName.toLowercase() === name.toLowerCase();
-  });
-}
+
 
 function findMatching (collection, name) {
   for (const name of collection) {
@@ -14,5 +10,5 @@ function findMatching (collection, name) {
     }
   }
 }
- 
+
 colorFilter(users, 'Red');
